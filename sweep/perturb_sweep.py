f32578f5eaeeede87e2e2f0dd1b873c0c4541f6f
Florian Burger <florian.burger@mpq.mpg.de>
2020-10-18 19:00:11 +0200

    changes to perturb_sweep.py after Id 8

--- sweep/perturb_sweep.py
+++ sweep/perturb_sweep.py
@@ -16,15 +16,13 @@ The h5py file created from this script contains two datasets :
 -------------------------------------------------------------------------------
 '''
 
-# data_file = "sub_perturb_yO_190.hdf5"             # Name of the file where the data will be stored
-# param_file = "sub_perturb_yO_190_param.txt" 
+data_file = "sub_perturb_yO_250.hdf5"             # Name of the file where the data will be stored
+param_file = "sub_perturb_yO_250_param.txt" 
 
-data_file = "test.h5"             # Name of the file where the data will be stored
-param_file = "test.txt" 
 
 # Name of the file where the parameters of interest will be stored     
 SUBSTRATE = True
-wvg_height = 0.19
+wvg_height = 0.25
 mode = "yO"
 
 #-----------------DEFAULTS-----------------------#
@@ -47,13 +45,13 @@ a_max = 0.45        # upper limit of the sweep of a
 w_min = 0.65         #  lower limit of w 
 w_max = 0.70        #  upper limit of w 
 
-hx_min = 0.15        # lower limit of the sweep of a
-hy_min = 0.225        #  lower limit of hy 
+hx_min = 0.125        # lower limit of the sweep of a
+hy_min = 0.200        #  lower limit of hy 
 
 hx_max = a_max - 0.07
 hy_max = w_max - 0.1  
 #------------------ Geometry Characteristics ---------------------#
-perturb_range = 0.04
+perturb_range = 0.05
 target_wvl = 1.54        # vaccum wavelength ( in um ) of the unperturbed cavity design
 
 f_perturb_lower = 1 / (target_wvl + perturb_range )           # target_f - perturbation
